694ee0ee073e30b5980e68e41820ca7e6be46457
VincentCYS <vincent.chengyunsheng@gmail.com>
2025-06-16 01:21:52 +0800

    added send message to game topic

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -66,6 +66,40 @@ def create_app(test_config=None):
       producer.flush()
 
       return ''
+    
+    @app.route('/add_game/<key>/<value>', methods=['POST'])
+    def add_game(key, value):
+      config = {
+          # User-specific properties that you must set
+          'bootstrap.servers': 'localhost:50841',
+
+          # Fixed properties
+          'acks': 'all'
+      }
+
+      # Create Producer instance
+      producer = Producer(config)
+
+      # Optional per-message delivery callback (triggered by poll() or flush())
+      # when a message has been successfully delivered or permanently
+      # failed delivery (after retries).
+      def delivery_callback(err, msg):
+          if err:
+              print('ERROR: Message failed delivery: {}'.format(err))
+          else:
+              print("Produced event to topic {topic}: key = {key:12} value = {value:12}".format(
+                  topic=msg.topic(), key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
+
+      # Produce data by selecting random values from these lists.
+      topic = "games"
+
+      producer.produce(topic, value, key, on_delivery=delivery_callback)
+
+      # Block until the messages are sent.
+      producer.poll(10000)
+      producer.flush()
+
+      return ''
 
     
 
