7696a5be70663e463b84dc937a3b78c4803bf376
VincentCYS <vincent.chengyunsheng@gmail.com>
2025-06-28 13:50:17 +0800

    removed file

--- kafka/ec2-user-data.sh
+++
@@ -1,6 +0,0 @@
- #!/bin/bash
- cd kafka
- cd kafka_2.13-4.0.0
- KAFKA_CLUSTER_ID="$(bin/kafka-storage.sh random-uuid)"
- bin/kafka-storage.sh format --standalone -t $KAFKA_CLUSTER_ID -c config/server.properties
- bin/kafka-server-start.sh config/server.properties
